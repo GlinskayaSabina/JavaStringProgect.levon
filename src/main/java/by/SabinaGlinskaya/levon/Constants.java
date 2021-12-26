2952bd9e17e79a99ced23644ce4f3c85a29bbc57
GlinskayaSabina <70802727+GlinskayaSabina@users.noreply.github.com>
2021-12-26 16:27:54 +0300

    fix

--- src/main/java/by/SabinaGlinskaya/levon/Constants.java
+++ src/main/java/by/SabinaGlinskaya/levon/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
     public static final String MY_EMAIL = "sabinagli45@gmail.com";
 
-    public static final String MY_PASSWORD = "10102001Ssa";
+    public static final String MY_PASSWORD = "";
 }
